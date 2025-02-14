f0a2d3e26ab1bd010623303f7184ba557033bf8b
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-02-14 21:44:22 +0530

    reordering the workshop

--- src/Pages/Workshops.js
+++ src/Pages/Workshops.js
@@ -95,6 +95,14 @@ const Workshops = () => {
       registrationLink: "",
     },
     {
+      title: "Archery: Sharpen Your Focus, Hit Your Precision Goals!",
+      date: "Date : 13th-Mar-2025",
+      time: "Time : 10:00 a.m. - 01:00 p.m.",
+      venue: "Venue : Basketball Court",
+      description: "Sportiva",
+      registrationLink: "",
+    },
+    {
       title: "Voice of Doctor",
       date: "Date : 27th-Mar-2025",
       time: "Time : 11:00 a.m. - 01:00 p.m.",
@@ -118,14 +126,7 @@ const Workshops = () => {
       description: "Sportiva",
       registrationLink: "",
     },
-    {
-      title: "Archery: Sharpen Your Focus, Hit Your Precision Goals!",
-      date: "Date : 13th-Mar-2025",
-      time: "Time : 10:00 a.m. - 01:00 p.m.",
-      venue: "Venue : Basketball Court",
-      description: "Sportiva",
-      registrationLink: "",
-    },
+   
   
     {
       title: "Application Containerization Using Docker",
