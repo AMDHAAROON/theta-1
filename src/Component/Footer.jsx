88ed147ec65d2e23d1c91a3ed0bb7fd96c3f78b7
Mohamed Haaroon A <mdhaaroon2020@gmail.com>
2025-03-13 11:01:29 +0530

    fixed the path

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 const navLinks = [
   { title: "Home", path: "/" },
   { title: "Workshops", path: "/Workshops" },
-  { title: "Events", path: "/Allevents" },
+  { title: "Events", path: "/allEvents" },
   { title: "Contact Us", path: "/contact-us" },
   
 ];
